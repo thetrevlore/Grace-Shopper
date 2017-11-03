382918f18d2da666d3d3ebe00bba99a6d4ad7dc2
Matthew Smith <mattsheridansmith@gmail.com>
2017-11-03 15:35:33 -0400

    [FEATURE] Cart: Initial commit

---
+++ client/components/checkout-page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import store from '../store/index';
+
+class StudentList extends Component {
+  constructor (props) {
+    super(props);
+    
+  }
+}
