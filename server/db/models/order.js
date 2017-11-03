68ff0b2fb5d8e32772bc1fd6502d8301e97b3a8d
Trevor Storey <trevorstorey23@gmail.com>
2017-11-03 16:07:52 -0400

    no change

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -3,7 +3,8 @@ const db = require('../db')
 
 const Order = db.define('order', {
   status: {
-    type: Sequelize.ENUM('Created', 'Processing', 'Cancelled', 'Completed')
+    type: Sequelize.ENUM('Created', 'Processing', 'Cancelled', 'Completed'),
+    defaultValue: 'Created'
   },
   shippingAddress: Sequelize.STRING,
   hasBeenPlaced: { type: Sequelize.BOOLEAN, defaultValue: false }
