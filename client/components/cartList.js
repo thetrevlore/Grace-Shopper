55f3bc22e355ad865276171bea1e3eca512534d6
Trevor Storey <trevorstorey23@gmail.com>
2017-11-06 16:55:37 -0500

    [Feature], cartlist, logic potentially done. implement removeOneFromCart

--- client/components/cartList.js
+++ client/components/cartList.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { updateInventoryThunk, removeOneFromCart } from '../store';
 
 const CartList = (props) => {
 
   const { products } = props
   const cartItems = props.items;
   const total = cartItems.reduce((acc, cur) => acc+=cur.quantity * cur.price, 0)
+  
 
-  console.log("CART ITEMS --- ", cartItems)
-  console.log("PRODUCTS ---", products)
   return (
     <div>
     <table>
@@ -23,16 +25,13 @@ const CartList = (props) => {
         {
           cartItems &&
           cartItems.map((item, idx) => {
+            const targetedProduct = products.filter(product => +product.id === +item.productId)[0];
              return (
               <tr key={idx}>
                 <td>{item.title}</td>
                 <td>{`$${+item.price * (+item.quantity)}.00`}</td>
-                <td>{item.quantity}</td>
-                
-                <td><button onClick={()=>{
-                  props.delete(item, products.filter(product => +product.id === +item.productId)[0])
-                }
-                }>&times;</button></td>
+                <td><button onClick={()=>{props.decrementQuantity(item, targetedProduct)}}>-</button> {item.quantity} <button onClick={props.incrementQuantity(item, targetedProduct)}>+</button></td>
+                <td><button onClick={()=>{props.delete(item, targetedProduct)}}>&times;</button></td>
               </tr>
             )
           })
@@ -43,6 +42,37 @@ const CartList = (props) => {
     </div>)
 }
 
-export default CartList;
 
+// const mapStateToProps = (state) => ({
+//   cartArray: Object.keys(state.cart).map(item => {
+//     state.cart[item].productId = +item
+//     return state.cart[item]
+//   }),
+//   user: state.user,
+//   products: state.products
+// });
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // need to make a 
+    // item???
+    // newQuantity???
+    // oldQuantity???
+    // ev.preventDefault() somewhere...
+    // YOU'RE ON MASTER RIGHT NOW. STOP!!!
+    decrementQuantity: (item, selectedProduct) => {
+        dispatch(removeOneFromCart(item.productId))
+        selectedProduct.inventoryAmount+=item.quantity
+        dispatch(updateInventoryThunk(selectedProduct))
+    },
+    incrementQuantity: (item, selectedProduct) => {
+        dispatch(addToCart(selectedProduct, quantity))
+        selectedProduct.inventoryAmount-=quantity
+        dispatch(updateInventoryThunk(selectedProduct))
+      }
+      
+    }
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CartList));
