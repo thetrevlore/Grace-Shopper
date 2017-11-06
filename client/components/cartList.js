c77475a29bfae8ff8680b78b60fc5b0770cbde92
Gabriel DeFazio <aloveleter@gmail.com>
2017-11-05 23:44:26 -0500

    first commit on this branch

--- client/components/cartList.js
+++ client/components/cartList.js
@@ -3,6 +3,7 @@ import React from 'react';
 const CartList = (props) => {
   const cartItems = props.items;
   const { products } = props
+  const total = cartItems.reduce((acc, cur) => cur.quantity * cur.price, 0)
 
   return (
     <div>
@@ -13,21 +14,26 @@ const CartList = (props) => {
           <th>Price</th>
           <th>Quantity</th>
           <th>Delete</th>
+          <th></th>
+          <th>Total</th>
         </tr>
       </thead>
       <tbody>
         {
           cartItems &&
           cartItems.map((item, idx) => {
+            console.log(item.price * item.quantity)
              return (
               <tr key={idx}>
                 <td>{item.title}</td>
-                <td>{item.price}</td>
+                <td>{item.price * item.quantity}</td>
                 <td>{item.quantity}</td>
                 <td><button onClick={()=>{
                   props.delete(item, products.filter(product => +product.id === +item.productId)[0])
                 }
                 }>&times;</button></td>
+                <td></td>
+                <td>{total}</td>
               </tr>
             )
           })
