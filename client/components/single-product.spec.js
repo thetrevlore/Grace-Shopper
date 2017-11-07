16dfc4424ea39b3f23bb3a0545adbe68af05aa4a
Trevor Storey <trevorstorey23@gmail.com>
2017-11-07 13:56:39 -0500

    [Fix], single-product.spec imports from correct file

--- client/components/single-product.spec.js
+++ client/components/single-product.spec.js
@@ -5,7 +5,7 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {SingleProduct} from './user-home'
+import {SingleProduct} from './single-product'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
